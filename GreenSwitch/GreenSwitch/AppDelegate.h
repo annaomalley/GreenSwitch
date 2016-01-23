7cb3b527d8a4311994a719478cc793b31cea88fa
miafg <miaf@sas.upenn.edu>
2016-01-23 13:02:10 -0500

    index.html change title

---
+++ GreenSwitch/GreenSwitch/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GreenSwitch
+//
+//  Created by Mia Froehling-Gallier on 1/23/16.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GreenSwitch/GreenSwitch/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  GreenSwitch
+//
+//  Created by Mia Froehling-Gallier on 1/23/16.
+//
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
